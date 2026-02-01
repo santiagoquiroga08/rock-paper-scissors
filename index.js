89fb18ade818679d4dbf8c03bf950d213fcfeb1f
santiagoquiroga08 <santiquiroga2002@gmail.com>
2026-02-01 16:12:02 -0500

    Implement 5-round game flow with score tracking

--- index.js
+++ index.js
@@ -10,15 +10,20 @@ function playRound(humanChoice, computerChoice){
         (humanChoice === "scissors" && computerChoice === "rock")
     ) {
         console.log("You lose, " + computerChoice + " beats " + humanChoice);
+        computerScore ++;
     } else if (
         (humanChoice === "rock" && computerChoice === "scissors") || 
         (humanChoice === "paper" && computerChoice === "rock") || 
         (humanChoice === "scissors" && computerChoice === "paper")
     ) {
-        console.log("You win, " + humanChoice +" beats " + computerChoice);   
+        console.log("You win, " + humanChoice + " beats " + computerChoice);
+        humanScore ++;   
     } 
 
 }
+
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice(){
     const choice = Math.floor(Math.random() * 3);
@@ -35,7 +40,27 @@ function getHumanChoice(){
     return prompt("Rock, paper or scissors.").toLowerCase();
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+const humanSelection1 = getHumanChoice();
+const computerSelection1 = getComputerChoice();
+playRound(humanSelection1, computerSelection1);
+console.log("Your score is " + humanScore + " computer score is " + computerScore);
 
-playRound(humanSelection, computerSelection);
+const humanSelection2 = getHumanChoice();
+const computerSelection2 = getComputerChoice();
+playRound(humanSelection2, computerSelection2);
+console.log("Your score is " + humanScore + " computer score is " + computerScore);
+
+const humanSelection3 = getHumanChoice();
+const computerSelection3 = getComputerChoice();
+playRound(humanSelection3, computerSelection3);
+console.log("Your score is " + humanScore + " computer score is " + computerScore);
+
+const humanSelection4 = getHumanChoice();
+const computerSelection4 = getComputerChoice();
+playRound(humanSelection4, computerSelection4);
+console.log("Your score is " + humanScore + " computer score is " + computerScore);
+
+const humanSelection5 = getHumanChoice();
+const computerSelection5 = getComputerChoice();
+playRound(humanSelection5, computerSelection5);
+console.log("Your score is " + humanScore + " computer score is " + computerScore);
