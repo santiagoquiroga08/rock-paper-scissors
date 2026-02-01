70226f65250f2ce2e1c4bbc2343500ef632ed490
santiagoquiroga08 <santiquiroga2002@gmail.com>
2026-02-01 16:32:13 -0500

    Create function playGame

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-function playRound(humanChoice, computerChoice){
+function playGame() {
+
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice){
     
     console.log("Play round: " + humanChoice + " " + computerChoice);
 
@@ -11,19 +16,38 @@ function playRound(humanChoice, computerChoice){
     ) {
         console.log("You lose, " + computerChoice + " beats " + humanChoice);
         computerScore ++;
-    } else if (
-        (humanChoice === "rock" && computerChoice === "scissors") || 
-        (humanChoice === "paper" && computerChoice === "rock") || 
-        (humanChoice === "scissors" && computerChoice === "paper")
-    ) {
+    } else {
         console.log("You win, " + humanChoice + " beats " + computerChoice);
         humanScore ++;   
     } 
 
-}
+    }
 
-let humanScore = 0;
-let computerScore = 0;
+    const humanSelection1 = getHumanChoice();
+    const computerSelection1 = getComputerChoice();
+    playRound(humanSelection1, computerSelection1);
+    console.log("Your score is " + humanScore + " computer score is " + computerScore);
+
+    const humanSelection2 = getHumanChoice();
+    const computerSelection2 = getComputerChoice();
+    playRound(humanSelection2, computerSelection2);
+    console.log("Your score is " + humanScore + " computer score is " + computerScore);
+
+    const humanSelection3 = getHumanChoice();
+    const computerSelection3 = getComputerChoice();
+    playRound(humanSelection3, computerSelection3);
+    console.log("Your score is " + humanScore + " computer score is " + computerScore);
+
+    const humanSelection4 = getHumanChoice();
+    const computerSelection4 = getComputerChoice();
+    playRound(humanSelection4, computerSelection4);
+    console.log("Your score is " + humanScore + " computer score is " + computerScore);
+
+    const humanSelection5 = getHumanChoice();
+    const computerSelection5 = getComputerChoice();
+    playRound(humanSelection5, computerSelection5);
+    console.log("Your score is " + humanScore + " computer score is " + computerScore);
+}
 
 function getComputerChoice(){
     const choice = Math.floor(Math.random() * 3);
@@ -40,27 +64,4 @@ function getHumanChoice(){
     return prompt("Rock, paper or scissors.").toLowerCase();
 }
 
-const humanSelection1 = getHumanChoice();
-const computerSelection1 = getComputerChoice();
-playRound(humanSelection1, computerSelection1);
-console.log("Your score is " + humanScore + " computer score is " + computerScore);
-
-const humanSelection2 = getHumanChoice();
-const computerSelection2 = getComputerChoice();
-playRound(humanSelection2, computerSelection2);
-console.log("Your score is " + humanScore + " computer score is " + computerScore);
-
-const humanSelection3 = getHumanChoice();
-const computerSelection3 = getComputerChoice();
-playRound(humanSelection3, computerSelection3);
-console.log("Your score is " + humanScore + " computer score is " + computerScore);
-
-const humanSelection4 = getHumanChoice();
-const computerSelection4 = getComputerChoice();
-playRound(humanSelection4, computerSelection4);
-console.log("Your score is " + humanScore + " computer score is " + computerScore);
-
-const humanSelection5 = getHumanChoice();
-const computerSelection5 = getComputerChoice();
-playRound(humanSelection5, computerSelection5);
-console.log("Your score is " + humanScore + " computer score is " + computerScore);
+playGame();
